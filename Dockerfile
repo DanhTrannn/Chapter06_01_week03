fdbb73d06af5d8c38239f8589d7d998cb0cce80f
DanhTrannn <tranthanhdanhktvn2005d@gmail.com>
2025-09-07 17:26:09 +0700

    1

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM tomcat:8.5-jdk8
+WORKDIR /usr/local/tomcat
+RUN rm -rf webapps/*
+COPY target/danhtran-1.0.war webapps/ROOT.war
+EXPOSE 8080
+CMD ["catalina.sh", "run"]
---
+++ src/main/java/com/danhtran/controller/EmailServlet.java
@@ -0,0 +1,75 @@
+package com.danhtran.controller;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import javax.servlet.http.HttpServlet;
+
+import com.danhtran.model.User;
+
+public class EmailServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+		
+		GregorianCalendar currentDate = new GregorianCalendar();
+		int currentYear = currentDate.get(Calendar.YEAR);
+		request.setAttribute("year", currentYear);
+//		request.getRequestDispatcher("footer.jsp").forward(request, response);
+		
+        String url = "/index.html";
+        
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";  // default action
+        }
+
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.jsp";    // the "join" page
+        } 
+        else if (action.equals("add")) {
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            // store data in User object
+            User user = new User(firstName, lastName, email);
+
+            // validate the parameters
+            String message;
+            if (firstName == null || lastName == null || email == null ||
+                firstName.isEmpty() || lastName.isEmpty() || email.isEmpty()) {
+                message = "Please fill out all three text boxes.";
+                url = "/index.jsp";
+            } 
+            else {
+                message = null;
+                url = "/thanks.jsp";
+            }
+            request.setAttribute("user", user);
+            request.setAttribute("message", message);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }    
+}
